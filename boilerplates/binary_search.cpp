4140253da3df173c3d3f336d14c04f774c976308
Noverish <embrapers263@gmail.com>
2021-02-07 17:44:16 +0900

    Modified binary_search snippet

--- boilerplates/binary_search.cpp
+++ boilerplates/binary_search.cpp
@@ -19,15 +19,19 @@ int custom_lower_bound(int left, int right, int val, Transform transform) {
 }
 
 int custom_upper_bound(int left, int right, int val, Transform transform) {
-    while(left < right) {
+    while(left <= right) {
         int mid = (left + right) / 2;
         if (val < transform(mid)) {
-            right = mid;
+            if (left == right) {
+                break;
+            } else {
+                right = mid;
+            }
         } else {
             left = mid + 1;
         }
     }
-    return left;
+    return left - 1;
 }
 
 int custom_lower_bound_descending(int left, int right, int val, Transform transform) {
@@ -43,15 +47,19 @@ int custom_lower_bound_descending(int left, int right, int val, Transform transf
 }
 
 int custom_upper_bound_descending(int left, int right, int val, Transform transform) {
-    while(left < right) {
+    while(left <= right) {
         int mid = (left + right) / 2;
         if (val > transform(mid)) {
-            right = mid;
+            if (left == right) {
+                break;
+            } else {
+                right = mid;
+            }
         } else {
             left = mid + 1;
         }
     }
-    return left;
+    return left - 1;
 }
 
 int main() {
@@ -63,23 +71,43 @@ int main() {
     cout << "lower index: " << distance(arr.begin(), lowerp) << "\n";
     cout << "upper index: " << distance(arr.begin(), upperp) << "\n";
     
-    // 나누어지는 수: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    //         몫: 0, 0, 1, 1, 1, 2, 2, 2, 3, 3
-    // 3으로 나눈 몫이 2인 수 중에 가장 작은 수 (6)
-    int num1 = custom_lower_bound(0, 10, 2, [](int a){ return a / 3; });
-    cout << "lower: " << num1 << "\n";
-    // 3으로 나눈 몫이 2인 수 중에 가장 큰 수 (8)
-    int num2 = custom_upper_bound(0, 10, 2, [](int a){ return a / 3; });
-    cout << "upper: " << num2 - 1 << "\n";
+    //     x: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    // x / 3: 0, 0, 1, 1, 1, 2, 2, 2, 3,  3
 
-    // 나누는수: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-    //     몫: 10, 5, 3, 2, 2, 1, 1, 1, 1,  1
-    // 몫이 2인 나누는 수 중에 제일 작은 수 (4)
-    int num3 = custom_lower_bound_descending(0, 10, 2, [](int a){ return 10 / a; });
-    cout << "lower: " << num3 << "\n";
-    // 몫이 2인 나누는 수 중에 제일 큰 수 (5)
-    int num4 = custom_upper_bound_descending(0, 10, 2, [](int a){ return 10 / a; });
-    cout << "upper: " << num4 - 1 << "\n";
+    // x / 3 = 2 에서 x 의 최솟값 (1 <= x <= 10)
+    int num11 = custom_lower_bound(1, 10, 2, [](int a){ return a / 3; });
+    cout << "lower: " << num11 << "\n"; // 6
+
+    // x / 3 = 0 에서 x 의 최솟값 (1 <= x <= 10)
+    int num12 = custom_lower_bound(1, 10, 0, [](int a){ return a / 3; });
+    cout << "lower: " << num12 << "\n"; // 1
+
+    // x / 3 = 2 에서 x 의 최댓값 (1 <= x <= 10)
+    int num21 = custom_upper_bound(1, 10, 2, [](int a){ return a / 3; });
+    cout << "upper: " << num21 << "\n"; // 8
+
+    // x / 3 = 3 에서 x 의 최댓값 (1 <= x <= 10)
+    int num22 = custom_upper_bound(1, 10, 3, [](int a){ return a / 3; });
+    cout << "upper: " << num22 << "\n"; // 10
+
+    //      x:  1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    // 10 / x: 10, 5, 3, 2, 2, 1, 1, 1, 1,  1
+
+    // 10 / x = 2 에서 x 의 최솟값 (1 <= x <= 10)
+    int num31 = custom_lower_bound_descending(1, 10, 2, [](int a){ return 10 / a; });
+    cout << "lower: " << num31 << "\n"; // 4
+
+    // 10 / x = 10 에서 x 의 최솟값 (1 <= x <= 10)
+    int num32 = custom_lower_bound_descending(1, 10, 10, [](int a){ return 10 / a; });
+    cout << "lower: " << num32 << "\n"; // 1
+
+    // 10 / x = 2 에서 x 의 최댓값 (1 <= x <= 10)
+    int num41 = custom_upper_bound_descending(0, 10, 2, [](int a){ return 10 / a; });
+    cout << "upper: " << num41 << "\n"; // 5
+
+    // 10 / x = 1 에서 x 의 최댓값 (1 <= x <= 10)
+    int num42 = custom_upper_bound_descending(0, 10, 1, [](int a){ return 10 / a; });
+    cout << "upper: " << num42 << "\n"; // 10
 
     return 0;
 }
